221b4507229e641e3f63ef76c5f3e8c2371303f2
junk dog <omaeadmi0909@gmail.com>
2022-05-22 13:44:28 +0900

    height の変更

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,6 +1,6 @@
 export const Hero: React.FC = () => (
   <>
-    <div className='bg-fit h-[700px] bg-hero bg-no-repeat text-primary-800'>
+    <div className='bg-fit h-[540px] bg-hero bg-no-repeat text-primary-800'>
       <div className='m-4 pt-8'>
         <p className='text-lg'>人を想う心を持った合理化</p>
         <p className='text-2xl font-bold '>
