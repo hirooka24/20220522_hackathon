9e865bb0c3bd251d932b88cecf89403633ab812c
junk dog <omaeadmi0909@gmail.com>
2022-05-22 11:42:34 +0900

    feat: PageHeader の作成完了

--- src/component/PagaHeader.tsx
+++ src/component/PagaHeader.tsx
@@ -1,1 +1,15 @@
-export const PageHeader: React.FC = () => <></>;
+export const PageHeader: React.FC = () => {
+  const muneList: string[] = ['Task', 'Solution', 'How to use'];
+  return (
+    <header className='bg-gradient-to-r from-primary-800 to-primary-500 text-white'>
+      <p className='text-center text-lg font-bold tracking-widest'>Hoshimo</p>
+      <ul className='flex items-center justify-center gap-8 text-base'>
+        {muneList.map((list, i) => (
+          <li key={i} className='hover:cursor-pointer hover:opacity-75'>
+            {list}
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
