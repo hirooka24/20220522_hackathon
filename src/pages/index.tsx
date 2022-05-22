b37e1e004105514736fe12540086f73509dea1c6
junk dog <omaeadmi0909@gmail.com>
2022-05-22 11:15:04 +0900

    feat: コンポーネントの空ファイル作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { Cta } from '@/component/Cta';
+import { Hero } from '@/component/Hero';
+import { HowToUse } from '@/component/HowToUse';
+import { PageHeader } from '@/component/PagaHeader';
+import { PageFooter } from '@/component/PageFooter';
+import { Solution } from '@/component/Solution';
+import { Task } from '@/component/Task';
+
 const Home: React.FC = () => (
   <main className='mx-auto max-w-6xl py-6'>
     <h1 className='text-lg font-bold text-primary-800'>
