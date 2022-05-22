2c03e4003c7e73a602864e65a0832b6f155b1097
junk dog <omaeadmi0909@gmail.com>
2022-05-22 13:43:27 +0900

    Cta の作成完了

--- src/component/Cta.tsx
+++ src/component/Cta.tsx
@@ -1,1 +1,14 @@
-export const Cta: React.FC = () => <></>;
+export const Cta: React.FC = () => (
+  <div className=' bg-primary-800'>
+    <div className='py-12 px-8 text-right'>
+      <h2 className='text-left text-lg font-bold text-white'>
+        世の中の<span className='text-xl'>もったいない</span>
+        <br />
+        <span className='text-xl'>ミスマッチ</span>をなくす
+      </h2>
+      <button className='mt-12 rounded-md bg-gray-100 px-6 py-3 font-bold tracking-widest text-primary-800 hover:opacity-70'>
+        資料請求
+      </button>
+    </div>
+  </div>
+);
