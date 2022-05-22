72702c3a868a9cd0f9a6072514314c3caa9d8646
junk dog <omaeadmi0909@gmail.com>
2022-05-22 16:13:29 +0900

    test

--- src/component/PagaHeader.tsx
+++ src/component/PagaHeader.tsx
@@ -1,14 +1,18 @@
+import { Link as Scroll } from 'react-scroll';
+
 export const PageHeader: React.FC = () => {
   const muneList: string[] = ['Task', 'Solution', 'How to use'];
-  const idList: string[] = ['#task', '#solution', '#howtouse'];
+  const idList: string[] = ['task', 'solution', 'howtouse'];
   return (
     <header className='bg-gradient-to-r from-primary-800 to-primary-500 py-4 text-white sm:flex sm:items-center sm:justify-around'>
       <p className='text-center text-lg font-bold tracking-widest'>Hoshimo</p>
       <ul className='mt-2 flex items-center justify-center gap-8 text-base'>
         {muneList.map((list, i) => (
-          <li key={i} className='scroll-smooth hover:cursor-pointer hover:opacity-75'>
-            <a href={idList[i]}>{list}</a>
-          </li>
+          <Scroll key={i} to={idList[i]} smooth={true} duration={500}>
+            <li key={i} className=' hover:cursor-pointer hover:opacity-75'>
+              {list}
+            </li>
+          </Scroll>
         ))}
       </ul>
     </header>
